f37b39f48ee80fe0f93aacefcd281b7a402e7aad
Alessandro Agnelli <68346635+Alexsandro91@users.noreply.github.com>
2021-01-10 22:57:53 +0100

    autentication and implement firebase

---
+++ src/pages/sign-page/sign-page.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+
+import './sign-page.styles.scss';
+
+const Sign = () => (
+    <div className = 'sign'>
+        <SignIn />
+        <SignUp />
+    </div>
+);
+
+export default Sign;
